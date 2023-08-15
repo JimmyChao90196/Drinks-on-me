9f307143c27a573b1471bcb65dd402e0986d0970
JimmyChao90196 <myname90196@gmail.com>
2023-08-15 15:32:49 +0800

    Initial Commit

---
+++ Drinks on me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drinks on me
+//
+//  Created by JimmyChao on 2023/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
